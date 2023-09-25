a99c434784dc7ca3b721708ea4df8c71b9477a2f
Rasmus Krogh <rasmus.kroghandersen@gmail.com>
2023-09-25 16:35:55 +0200

    building an express server

--- index.js
+++ index.js
@@ -1,46 +1,20 @@
-const http = require("http");
-const fs = require("fs");
+const morgan = require("morgan");
+const express = require("express");
+const app = express();
+const saltLogger = function (req, res, next) {
+  console.log(`salt> ${req.method} - ${req.url}`);
+  next();
+};
 
-const server = http.createServer((req, res) => {
-  //console.log(`The URL for the request was '${req.url}`);
-  //console.log(`The method for the request was '${req.method}`);
-  //console.log(res);
+app.use(saltLogger);
+app.use(morgan("tiny"));
+app.use(express.static("static"));
 
-  const fileNameOfUrl = (url) => {
-    let fileName = "";
-    if (req.url.split("/")[1] === "") {
-      fileName = "index.html";
-    } else {
-      fileName = req.url.split("/")[1];
-    }
-    return fileName;
-  };
-
-  const fileName = fileNameOfUrl(req.url);
-  if (fileName === "favicon.ico") {
-    res.statusCode = 404;
-    res.end("");
-    return;
-  }
-
-  const getFileContentOr404 = (fileName) => {
-    if (!fs.existsSync(`./static/${fileName}`)) {
-      fileName = "404.html";
-    }
-
-    return fs.readFileSync(`./static/${fileName}`, "utf-8");
-  };
-
-  const content = getFileContentOr404(fileName);
-
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end(content);
+app.get("/", (req, res) => {
+  res.send("Hello fellow developer!");
 });
 
-const hostname = "localhost";
 const port = 3000;
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
